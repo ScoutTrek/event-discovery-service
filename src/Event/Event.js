cb444c6afdea0f480787f8979659df176036ef00
Jason Cheung <59743969+Jason0429@users.noreply.github.com>
2022-11-20 16:10:32 -0500

    added more Event resolvers

--- src/Event/Event.js
+++ src/Event/Event.js
@@ -9,326 +9,320 @@ const { authenticated } = require("../utils/Auth");
 const GraphQLJSON = require("graphql-type-json");
 
 export const weekDays = {
-  Sunday: 0,
-  Monday: 1,
-  Tuesday: 2,
-  Wednesday: 3,
-  Thursday: 4,
-  Friday: 5,
-  Saturday: 6,
+	Sunday: 0,
+	Monday: 1,
+	Tuesday: 2,
+	Wednesday: 3,
+	Thursday: 4,
+	Friday: 5,
+	Saturday: 6
 };
 
 export const getInitialDate = (target, current) => {
-  target = +target;
-  current = +current;
-  if (target === current) {
-    return 7;
-  }
-  if (target > current) {
-    return target - current;
-  } else {
-    return 7 + target - current;
-  }
+	target = +target;
+	current = +current;
+	if (target === current) {
+		return 7;
+	}
+	if (target > current) {
+		return target - current;
+	} else {
+		return 7 + target - current;
+	}
 };
 
 export const typeDefs = gql`
-  scalar JSON
+	scalar JSON
 
-  enum EVENT_TYPES {
-    AQUATIC_EVENT
-    BACKPACK_TRIP
-    BIKE_RIDE
-    BOARD_OF_REVIEW
-    CAMPOUT
-    CANOE_TRIP
-    COMMITTEE_MEETING
-    CUSTOM_EVENT
-    EAGLE_PROJECT
-    FISHING_TRIP
-    FLAG_RETIREMENT
-    FUNDRAISER
-    TROOP_MEETING
-    HIKE
-    KAYAK_TRIP
-    MERIT_BADGE_CLASS
-    PARENT_MEETING
-    SCOUTMASTER_CONFERENCE
-    SERVICE_PROJECT
-    SPECIAL_EVENT
-    SUMMER_CAMP
-    SWIM_TEST
-  }
+	enum EVENT_TYPES {
+		AQUATIC_EVENT
+		BACKPACK_TRIP
+		BIKE_RIDE
+		BOARD_OF_REVIEW
+		CAMPOUT
+		CANOE_TRIP
+		COMMITTEE_MEETING
+		CUSTOM_EVENT
+		EAGLE_PROJECT
+		FISHING_TRIP
+		FLAG_RETIREMENT
+		FUNDRAISER
+		TROOP_MEETING
+		HIKE
+		KAYAK_TRIP
+		MERIT_BADGE_CLASS
+		PARENT_MEETING
+		SCOUTMASTER_CONFERENCE
+		SERVICE_PROJECT
+		SPECIAL_EVENT
+		SUMMER_CAMP
+		SWIM_TEST
+	}
 
-  # not currently in use
-  type Roster {
-    id: ID!
-    groups: [Troop]!
-    patrols: [Patrol]
-    individuals: [User]
-  }
-  input AddRosterInput {
-    groups: [ID]!
-    patrols: [ID]
-    individuals: [ID]
-  }
+	# not currently in use
+	type Roster {
+		id: ID!
+		groups: [Troop]!
+		patrols: [Patrol]
+		individuals: [User]
+	}
+	input AddRosterInput {
+		groups: [ID]!
+		patrols: [ID]
+		individuals: [ID]
+	}
 
-  type Event {
-    id: ID!
+	type Event {
+		id: ID!
 
-    createdAt: String!
-    updatedAt: String!
+		createdAt: String!
+		updatedAt: String!
 
-    patrol: Patrol
-    troop: Troop
+		patrol: Patrol
+		troop: Troop
 
-    type: EVENT_TYPES!
-    invited: [Roster]
-    attending: [Roster]
+		type: EVENT_TYPES!
+		invited: [Roster]
+		attending: [Roster]
 
-    title: String!
-    description: String
+		title: String!
+		description: String
 
-    date: String!
-    startTime: String!
-    meetTime: String
-    leaveTime: String
-    endTime: String
-    endDate: String
-    pickupTime: String
+		date: String!
+		startTime: String!
+		meetTime: String
+		leaveTime: String
+		endTime: String
+		endDate: String
+		pickupTime: String
 
-    uniqueMeetLocation: String
-    location: Location
-    meetLocation: Location
+		uniqueMeetLocation: String
+		location: Location
+		meetLocation: Location
 
-    checkoutTime: String
-    distance: Int
+		checkoutTime: String
+		distance: Int
 
-    published: Boolean
-    creator: User
-  }
+		published: Boolean
+		creator: User
+	}
 
-  input AddEventInput {
-    type: EVENT_TYPES!
-    invited: [AddRosterInput]
-    attending: [AddRosterInput]
+	input AddEventInput {
+		type: EVENT_TYPES!
+		invited: [AddRosterInput]
+		attending: [AddRosterInput]
 
-    title: String
-    description: String
+		title: String
+		description: String
 
-    date: String!
-    startTime: String!
-    meetTime: String
-    leaveTime: String
-    endTime: String
-    endDate: String
-    pickupTime: String
+		date: String!
+		startTime: String!
+		meetTime: String
+		leaveTime: String
+		endTime: String
+		endDate: String
+		pickupTime: String
 
-    uniqueMeetLocation: String
-    location: UpdateLocationInput
-    meetLocation: UpdateLocationInput
+		uniqueMeetLocation: String
+		location: UpdateLocationInput
+		meetLocation: UpdateLocationInput
 
-    checkoutTime: String
-    distance: Int
+		checkoutTime: String
+		distance: Int
 
-    published: Boolean
-  }
+		published: Boolean
+	}
 
-  input UpdateEventInput {
-    type: EVENT_TYPES
-    invited: [AddRosterInput]
-    attending: [AddRosterInput]
+	input UpdateEventInput {
+		type: EVENT_TYPES
+		invited: [AddRosterInput]
+		attending: [AddRosterInput]
 
-    title: String!
-    description: String
+		title: String!
+		description: String
 
-    date: String!
-    startTime: String!
-    uniqueMeetLocation: String
-    meetTime: String
-    leaveTime: String
-    endTime: String
-    endDate: String
-    pickupTime: String
+		date: String!
+		startTime: String!
+		uniqueMeetLocation: String
+		meetTime: String
+		leaveTime: String
+		endTime: String
+		endDate: String
+		pickupTime: String
 
-    location: UpdateLocationInput
-    meetLocation: UpdateLocationInput
+		location: UpdateLocationInput
+		meetLocation: UpdateLocationInput
 
-    checkoutTime: String
-    distance: Int
+		checkoutTime: String
+		distance: Int
 
-    published: Boolean
-  }
+		published: Boolean
+	}
 
-  type Location {
-    lat: Float!
-    lng: Float!
-    address: String
-  }
+	type Location {
+		lat: Float!
+		lng: Float!
+		address: String
+	}
 
-  input AddLocationInput {
-    lat: Float!
-    lng: Float!
-    address: String
-  }
+	input AddLocationInput {
+		lat: Float!
+		lng: Float!
+		address: String
+	}
 
-  input UpdateLocationInput {
-    lat: Float
-    lng: Float
-    address: String
-  }
+	input UpdateLocationInput {
+		lat: Float
+		lng: Float
+		address: String
+	}
 
-  extend type Query {
-    events(first: Int, skip: Int): [Event]
-    event(id: ID!): Event
-    eventSchemas: JSON
-  }
+	extend type Query {
+		events(first: Int, skip: Int): [Event]
+		event(id: ID!): Event
+		eventSchemas: JSON
+	}
 
-  extend type Mutation {
-    addEvent(input: AddEventInput!): Event
-    updateEvent(input: UpdateEventInput, id: ID!): Event
-    deleteEvent(id: ID!): Event
-  }
+	extend type Mutation {
+		addEvent(input: AddEventInput!): Event
+		updateEvent(input: UpdateEventInput, id: ID!): Event
+		deleteEvent(id: ID!): Event
+	}
 `;
 
 export const resolvers = {
-  JSON: GraphQLJSON,
-  Event: {
-    troop: async (parent, __, { TroopModel }) => {
-      await TroopModel.findById(parent.troop);
-    },
-    patrol: async (parent, __, { TroopModel }) => {
-      const myTroop = await TroopModel.findById(parent.troop);
-      const myPatrol = await myTroop.patrols.id(parent.patrol);
-      return myPatrol;
-    },
-    creator: async (parent, __, { UserModel }) =>
-      await UserModel.findById(parent.creator),
-    location: (parent) => {
-      if (parent.location) {
-        return {
-          lng: parent.location.coordinates[0],
-          lat: parent.location.coordinates[1],
-          address: parent.location.address,
-        };
-      }
-      return null;
-    },
-    meetLocation: (parent) => {
-      if (parent.meetLocation) {
-        return {
-          lng: parent.meetLocation.coordinates[0],
-          lat: parent.meetLocation.coordinates[1],
-          address: parent.meetLocation.address,
-        };
-      }
-      return null;
-    },
-  },
-  Query: {
-    // events: authenticated(
-    //   async (_, { first, skip }, { EventModel, currMembership }) => {
-    //     const events = await EventModel.find(
-    //       {
-    //         date: {
-    //           $gte: new Date(Date.now() - 86400000 * 1.5),
-    //           $lte: new Date(Date.now() + 6.04e8 * 10),
-    //         },
-    //         troop: currMembership?.troopID,
-    //       },
-    //       null,
-    //       {
-    //         first,
-    //         skip,
-    //       }
-    //     ).sort({ date: 1 });
-    //     return events;
-    //   }
-    // ),
-    // event: async (_, { id }, { EventModel }) => await EventModel.findById(id),
-    eventSchemas: () => EventSchemas,
-  },
-  Mutation: {
-    deleteEvent: authenticated(
-      async (_, { id }, { EventModel }) => await EventModel.findByIdAndDelete(id)
-    ),
-    addEvent: authenticated(
-      async (_, { input }, { EventModel, user, tokens, currMembership }) => {
-        if (input.type === "TROOP_MEETING") {
-          input.title = "Troop Meeting";
-        }
+	JSON: GraphQLJSON,
+	Event: {
+		/** TODO: WILL HAVE @FieldResolver annotations */
+		troop: async (parent, __, { TroopModel }) => {
+			await TroopModel.findById(parent.troop);
+		},
+		patrol: async (parent, __, { TroopModel }) => {
+			const myTroop = await TroopModel.findById(parent.troop);
+			const myPatrol = await myTroop.patrols.id(parent.patrol);
+			return myPatrol;
+		},
+		creator: async (parent, __, { UserModel }) => await UserModel.findById(parent.creator),
+		location: (parent) => {
+			if (parent.location) {
+				return {
+					lng: parent.location.coordinates[0],
+					lat: parent.location.coordinates[1],
+					address: parent.location.address
+				};
+			}
+			return null;
+		},
+		meetLocation: (parent) => {
+			if (parent.meetLocation) {
+				return {
+					lng: parent.meetLocation.coordinates[0],
+					lat: parent.meetLocation.coordinates[1],
+					address: parent.meetLocation.address
+				};
+			}
+			return null;
+		}
+	},
+	Query: {
+		// events: authenticated(
+		//   async (_, { first, skip }, { EventModel, currMembership }) => {
+		//     const events = await EventModel.find(
+		//       {
+		//         date: {
+		//           $gte: new Date(Date.now() - 86400000 * 1.5),
+		//           $lte: new Date(Date.now() + 6.04e8 * 10),
+		//         },
+		//         troop: currMembership?.troopID,
+		//       },
+		//       null,
+		//       {
+		//         first,
+		//         skip,
+		//       }
+		//     ).sort({ date: 1 });
+		//     return events;
+		//   }
+		// ),
+		// event: async (_, { id }, { EventModel }) => await EventModel.findById(id),
+		// eventSchemas: () => EventSchemas
+	},
+	Mutation: {
+		// deleteEvent: authenticated(
+		// 	async (_, { id }, { EventModel }) => await EventModel.findByIdAndDelete(id)
+		// ),
+		addEvent: authenticated(async (_, { input }, { EventModel, user, tokens, currMembership }) => {
+			if (input.type === "TROOP_MEETING") {
+				input.title = "Troop Meeting";
+			}
 
-        // combines the event date and time into one date object
-        // could be turned into a utility function in the future
-        let startDatetime = new Date(input?.meetTime || input?.startTime);
-        const eventDate = new Date(input?.date);
-        startDatetime.setMonth(eventDate.getMonth());
-        startDatetime.setDate(eventDate.getDate());
-        const mutationObject = {
-          ...input,
-          troop: currMembership?.troopID,
-          patrol: currMembership?.patrolID,
-          creator: user.id,
-          notification: startDatetime - 86400000,
-        };
+			// combines the event date and time into one date object
+			// could be turned into a utility function in the future
+			let startDatetime = new Date(input?.meetTime || input?.startTime);
+			const eventDate = new Date(input?.date);
+			startDatetime.setMonth(eventDate.getMonth());
+			startDatetime.setDate(eventDate.getDate());
+			const mutationObject = {
+				...input,
+				troop: currMembership?.troopID,
+				patrol: currMembership?.patrolID,
+				creator: user.id,
+				notification: startDatetime - 86400000
+			};
 
-        if (input.location) {
-          mutationObject.location = {
-            type: "Point",
-            coordinates: [input.location.lng, input.location.lat],
-            address: input.location.address,
-          };
-        }
-        if (input.meetLocation) {
-          mutationObject.meetLocation = {
-            type: "Point",
-            coordinates: [input.meetLocation.lng, input.meetLocation.lat],
-            address: input.meetLocation.address,
-          };
-        }
+			if (input.location) {
+				mutationObject.location = {
+					type: "Point",
+					coordinates: [input.location.lng, input.location.lat],
+					address: input.location.address
+				};
+			}
+			if (input.meetLocation) {
+				mutationObject.meetLocation = {
+					type: "Point",
+					coordinates: [input.meetLocation.lng, input.meetLocation.lat],
+					address: input.meetLocation.address
+				};
+			}
 
-        const event = await EventModel.create(mutationObject);
+			const event = await EventModel.create(mutationObject);
 
-        sendNotifications(
-          tokens,
-          `${input.title} event has been created. See details.`,
-          { type: "event", eventType: event.type, ID: event.id }
-        );
-        return event;
-      }
-    ),
-    updateEvent: authenticated(async (_, { input, id }, { EventModel, tokens }) => {
-      const newVals = { ...input };
-      if (input.location) {
-        newVals.location = {
-          type: "Point",
-          coordinates: [input.location.lng, input.location.lat],
-          address: input.location.address,
-        };
-      }
-      if (input.meetLocation) {
-        newVals.meetLocation = {
-          type: "Point",
-          coordinates: [input.meetLocation.lng, input.meetLocation.lat],
-          address: input.meetLocation.address,
-        };
-      }
+			sendNotifications(tokens, `${input.title} event has been created. See details.`, {
+				type: "event",
+				eventType: event.type,
+				ID: event.id
+			});
+			return event;
+		}),
+		updateEvent: authenticated(async (_, { input, id }, { EventModel, tokens }) => {
+			const newVals = { ...input };
+			if (input.location) {
+				newVals.location = {
+					type: "Point",
+					coordinates: [input.location.lng, input.location.lat],
+					address: input.location.address
+				};
+			}
+			if (input.meetLocation) {
+				newVals.meetLocation = {
+					type: "Point",
+					coordinates: [input.meetLocation.lng, input.meetLocation.lat],
+					address: input.meetLocation.address
+				};
+			}
 
-      await EventModel.updateOne({ _id: id }, newVals, {
-        new: true,
-      });
+			await EventModel.updateOne({ _id: id }, newVals, {
+				new: true
+			});
 
-      const updatedEvent = await EventModel.findById(id);
+			const updatedEvent = await EventModel.findById(id);
 
-      sendNotifications(
-        tokens,
-        `${updatedEvent.title} event has been updated!`,
-        {
-          type: "event",
-          eventType: updatedEvent.type,
-          ID: updatedEvent.id,
-        }
-      );
-      return updatedEvent;
-    }),
-  },
+			sendNotifications(tokens, `${updatedEvent.title} event has been updated!`, {
+				type: "event",
+				eventType: updatedEvent.type,
+				ID: updatedEvent.id
+			});
+			return updatedEvent;
+		})
+	}
 };
--- src/resolvers/event.ts
+++ src/resolvers/event.ts
@@ -1,9 +1,92 @@
-import mongoose from 'mongoose';
+import mongoose, { ObjectId } from 'mongoose';
 import { Arg, Authorized, Ctx, Int, Field, FieldResolver, ID, InputType, Mutation, Query, Resolver, Root } from 'type-graphql';
 import { Event } from '../../models/Event'
 import type { ContextType } from "../server";
+import EventSchemas from "../Event/EventSchemas.json";
+import { EventModel } from 'models/models';
 
-@Resolver(of => Event) 
+export type EventType =
+  "AQUATIC_EVENT"
+  | "BACKPACK_TRIP"
+  | "BIKE_RIDE"
+  | "BOARD_OF_REVIEW"
+  | "CAMPOUT"
+  | "CANOE_TRIP"
+  | "COMMITTEE_MEETING"
+  | "CUSTOM_EVENT"
+  | "EAGLE_PROJECT"
+  | "FISHING_TRIP"
+  | "FLAG_RETIREMENT"
+  | "FUNDRAISER"
+  | "TROOP_MEETING"
+  | "HIKE"
+  | "KAYAK_TRIP"
+  | "MERIT_BADGE_CLASS"
+  | "PARENT_MEETING"
+  | "SCOUTMASTER_CONFERENCE"
+  | "SERVICE_PROJECT"
+  | "SPECIAL_EVENT"
+  | "SUMMER_CAMP"
+  | "SWIM_TEST";
+
+export interface EventMetadata {
+  eventType: EventType;
+}
+
+@InputType()
+class AddRosterInput {
+  @Field(type => [ID])
+  id!: ObjectId[];
+  @Field(type => [ID])
+  patrols!: ObjectId[];
+  @Field(type => [ID])
+  individuals!: ObjectId[];
+}
+
+@InputType()
+class AddEventInput {
+  @Field()
+  type!: string;
+  @Field(type => [AddRosterInput])
+  invited!: AddRosterInput[];
+  @Field(type => [AddRosterInput])
+  attending!: AddRosterInput[];
+  @Field(type => String)
+  title!: string;
+  @Field(type => String)
+  description!: string;
+  @Field(type => Date)
+  date!: Date;
+  @Field(type => Date)
+  startTime!: Date;
+  @Field(type => Date)
+  meetTime!: Date;
+  @Field(type => Date)
+  leaveTime!: Date;
+  @Field(type => Date)
+  endTime!: Date;
+  @Field(type => Date)
+  pickupTime!: Date;
+  @Field(type => String)
+  uniqueMeetLocation!: string;
+  @Field(type => UpdateLocationInput)
+  location!: UpdateLocationInput;
+  @Field(type => UpdateLocationInput)
+  meetLocation!: UpdateLocationInput;
+  @Field(type => Date)
+  checkoutTime!: Date;
+  @Field(type => Int)
+  distance!: number;
+  @Field(type => Boolean)
+  published!: boolean;
+}
+
+@InputType()
+class UpdateLocationInput {
+  // TODO
+}
+
+@Resolver(of => Event)
 export class EventResolver {
 
   @Authorized()
@@ -14,7 +97,7 @@ export class EventResolver {
   ): Promise<Event> {
     const event = await ctx.EventModel.findById(id);
     if (!event) {
-      throw new Error("User could not be found")
+      throw new Error("Event could not be found");
     }
     return event;
   }
@@ -22,8 +105,8 @@ export class EventResolver {
   @Authorized()
   @Query(returns => [Event])
   async events(
-    @Arg("first", type => Int, {nullable: true}) first: number,
-    @Arg("skip", type => Int, {nullable: true}) skip: number,
+    @Arg("first", type => Int, { nullable: true }) first: number,
+    @Arg("skip", type => Int, { nullable: true }) skip: number,
     @Ctx() ctx: ContextType,
   ): Promise<Event[]> {
     if (ctx.currMembership === undefined) {
@@ -51,4 +134,28 @@ export class EventResolver {
 
     return events;
   }
+
+  @Query(returns => JSON)
+  eventSchemas(): any {
+    // TODO: fix return type once jaron is done correcting the type
+    return EventSchemas;
+  }
+
+  @Authorized()
+  @Mutation(returns => ID)
+  async deleteEvent(
+    @Arg("id", type => ID) id: string,
+    @Ctx() ctx: ContextType
+  ): Promise<Event | null> {
+    // TODO: double check `id` parameter type and Promise return type
+    return await ctx.EventModel.findByIdAndDelete(id);
+  }
+
+  // @Authorized()
+  // @Mutation(returns => Event)
+  // async addEvent(
+  //   input: AddEventInput
+  // ): Promise<Event> {
+
+  // }
 }
